c86641ce31d30ec3c42cd27352d7c1b21b6e3da2
gerrity95 <gerrity95@gmail.com>
2016-07-25 20:31:54 +0000

    Split Some of the code into classes, but still a lot remains in the main class... that can be worked on in the future but at the moment the aim is to make the origin spot the users current location

---
+++ app/src/main/java/com/example/mark/mapapp/MappingPoints.java
@@ -0,0 +1,36 @@
+package com.example.mark.mapapp;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by gerrity95 on 24/07/16.
+ */
+public class MappingPoints {
+
+
+    String getUrl(LatLng origin, LatLng dest) {
+
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+
+
+        // Sensor enabled
+        String sensor = "sensor=false";
+
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + sensor;
+
+        // Output format
+        String output = "json";
+
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
+
+
+        return url;
+    }
+
+}
